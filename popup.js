5e512b7a91524dfcec9c1660fbab7a09c55dcbbb
Michelle Curry <michellelicurry@icloud.com>
2022-12-21 15:37:30 -0500

    popup eventlistener

--- popup.js
+++ popup.js
@@ -1,5 +1,18 @@
 let scrapeCalendly = document.getElementById('scrapeCalendly');
 
-scrapeCalendly.addEventListener("click", () => {
-    alert('hello');
+scrapeCalendly.addEventListener("click", async () => {
+    
+    // Get current active tab
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true})
+
+    //Execute script to parse appointments on page
+    chrome.scripting.excuteScript({
+        target: {tabId: tab.id},
+        func: scrapeAppointmentsFromPage,
+    })
 })
+
+//Function to scrape appointments
+function scrapeAppointmentsFromPage() {
+    alert('hi');
+}
